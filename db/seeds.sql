ef93191bfd66a51a93b78b2f6c4501c73874a917
Peter Le <Peter Le@LC-PETERLAPTOP.lec.corp.com>
2018-02-26 18:29:57 -0600

    base things done. Need to get handlebars to display. base routing done.

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+insert into burgers(burger_name, devoured)
+values("Hamburger", false);
+
+insert into burgers(burger_name, devoured)
+values("Cheeseburger", false);
+
+insert into burgers(burger_name, devoured)
+values("Bacon Cheeseburger", false);
+
+insert into burgers(burger_name, devoured)
+values("Avacado Burger", false);
+
+insert into burgers(burger_name, devoured)
+values("BLT", false);
